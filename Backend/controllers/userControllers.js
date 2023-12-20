68a5da1028a8280d9bbd910ba9803f5f7ba44e13
Maziar Pedrami <awildroot@gmail.com>
2023-12-20 16:42:06 +0100

    refactored errorHandler and installed asyncHandler

--- Backend/controllers/userControllers.js
+++ Backend/controllers/userControllers.js
@@ -1,4 +1,5 @@
 const User = require("../models/user");
+const asyncHander = require("express-async-handler");
 
 const getUsers = async (req, res, next) => {
   try {
@@ -9,29 +10,23 @@ const getUsers = async (req, res, next) => {
   }
 };
 
-const addUser = async (req, res, next) => {
-  try {
-    const {
-      body: { username, password, email, role, avatar },
-    } = req;
-    const found = await User.findOne({ email });
-    if (found) {
-      res
-        .status(400)
-        .send({ message: `user already exist with email: ${email}` });
-      return;
-    }
-    const newUser = await User.create({
-      username,
-      password,
-      email,
-      role,
-      avatar,
-    });
-    res.json({ message: "new user created", data: newUser });
-  } catch (error) {
-    next(error);
+const addUser = asyncHander(async (req, res, next) => {
+  const {
+    body: { username, password, email, role, avatar },
+  } = req;
+  const found = await User.findOne({ email });
+  if (found) {
+    res.status(400);
+    throw new Error(`A user already exist with email: ${email}`);
   }
-};
+  const newUser = await User.create({
+    username,
+    password,
+    email,
+    role,
+    avatar,
+  });
+  res.json({ message: "new user created", data: newUser });
+});
 
 module.exports = { addUser, getUsers };
--- Backend/middlewares/errorHandler.js
+++ Backend/middlewares/errorHandler.js
@@ -1,15 +1,11 @@
 const errorHandler = (err, req, res, next) => {
-  console.log(err.stack);
-  if (res.headersSent) {
-    return next(err);
-  }
-  const statusCode = err.status || 500;
-  const errorMessage =
-    statusCode === 500 ? "Internal Server Error" : err.message;
+  const statusCode = res.statusCode ? res.statusCode : 500;
 
-  res.status(statusCode).json({ error: errorMessage });
-
-  /* res.status(500).json({ error: "Something went wrong" }); */
+  res.status(statusCode);
+  res.json({
+    message: err.message,
+    stack: err.stack,
+  });
 };
 
 module.exports = errorHandler;
