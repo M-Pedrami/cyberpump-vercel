cf1034cc76160b7bdcce14894f8e45f63006fb1f
Maziar Pedrami <awildroot@gmail.com>
2024-01-04 12:17:12 +0100

    WorkoutPage/Excerise-->successfully retrieved and displayed data

--- client/src/components/Exercise/index.jsx
+++ client/src/components/Exercise/index.jsx
@@ -1,41 +1,32 @@
-import Video1 from "../../assets/male-cable-twisting-curl-front.mp4";
-import Video2 from "../../assets/male-cable-twisting-curl-side.mp4";
-export default function Exercise() {
+import { useEffect } from "react";
+
+export default function Exercise({exercise}) {
+  console.log(exercise)
   return (
     <section className="">
       <div className="Card flex flex-col text-left p-10 w-3/4 m-auto">
         <div className="CardHeader bg-gradient-to-r from-deep-orange-700 to-deep-orange-400 p-4 w-full rounded-2xl">
           <h1 className="text-4xl text-white text-left font-bold">
-            {" "}
-            Cable Twisting Curl
+            {exercise?.name}
+            
           </h1>
         </div>
         <div className="CardVideos flex space-x-0 justify-evenly p-5 gap-4 mt-4 ">
-          <video
-            src={Video1}
+          {exercise?.video.map(video=>(<video
+            src={video}
             autoPlay
             muted
             loop
             className=" w-1/2 rounded-3xl border-t-4 border-b-8 border-orange-700  "
-          ></video>
-          <video
-            src={Video2}
-            autoPlay
-            muted
-            loop
-            className=" w-1/2 rounded-3xl border-t-8 border-b-4 border-orange-700 "
-          ></video>
+          ></video>))}
+          
         </div>
         <div className="CardSteps flex flex-col gap-2 text-lg text-white">
-          <p>
-            <span className="italic text-lg text-deep-orange-700 font-body">Step 1: </span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo,
+          {exercise?.instructions.map((instruction, index)=>( <p>
+            <span className="italic text-lg text-deep-orange-700 font-body">Step {index+1}: </span>{instruction}
             itaque.
-          </p>
-          <p>
-          <span className="italic text-lg text-deep-orange-700 font-body">Step 2: </span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam ipsa
-            amet similique neque rerum dolore!
-          </p>
-          <p><span className="italic text-lg text-deep-orange-700 font-body">Step 3: </span>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </p>))}
+          
         </div>
       </div>
     </section>
--- client/src/components/WorkoutPage/index.jsx
+++ client/src/components/WorkoutPage/index.jsx
@@ -1,28 +1,24 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Exercise from "../Exercise";
 import { GrCaretNext } from "react-icons/gr";
 import { GrCaretPrevious } from "react-icons/gr";
 
-
+import getExercises from "../../utils/customrHooks";
 
 export default function WorkoutPage() {
   const [currentPage, setCurrentPage] = useState(1);
+  const [exercisesData, setExercisesData] = useState([]);
   const exercisesPerPage = 1;
 
-  // Array of exercise objects, you can add more exercises as needed
-  const exercisesData = [
-    {
-      id: 1,
-      title: "Exercise 1",
-      description: "Description for Exercise 1",
-    },
-    {
-      id: 2,
-      title: "Exercise 2",
-      description: "Description for Exercise 2",
-    },
-    // Add more exercises as needed
-  ];
+  //getting the exercises from backend
+  useEffect(() => {
+    getExercises()
+      .then((res) => {
+        setExercisesData(res.exercises);
+        console.log("USEEFFECT", res.exercises);
+      })
+      .catch((err) => console.log("USEEFFECT", err));
+  }, []);
 
   const totalExercises = exercisesData.length;
   const totalPages = Math.ceil(totalExercises / exercisesPerPage);
@@ -37,23 +33,34 @@ export default function WorkoutPage() {
 
   const startIndex = (currentPage - 1) * exercisesPerPage;
   const endIndex = Math.min(startIndex + exercisesPerPage, totalExercises);
-  const exercisesToDisplay = exercisesData.slice(startIndex, endIndex);
+  const exercisesToDisplay = exercisesData?.slice(startIndex, endIndex);
 
   return (
     <section className="p-6">
       <div className=" mt-10 mb-0 flex items-center justify-center gap-4 grow-0">
-      <button className="w-10" onClick={goToPreviousPage} disabled={currentPage === 1}>
-        <GrCaretPrevious className="text-5xl  text-deep-orange-700 hover:text-4xl transition-all duration-500 hover:text-opacity-50"/>
+        <button
+          className="w-10"
+          onClick={goToPreviousPage}
+          disabled={currentPage === 1}
+        >
+          <GrCaretPrevious className="text-5xl  text-deep-orange-700 hover:text-4xl transition-all duration-500 hover:text-opacity-50" />
         </button>
-        <span className="text-xl font-bold text-white  "> Exercise {currentPage} of {totalPages} </span>
-        <button className="w-10" onClick={goToNextPage} disabled={currentPage === totalPages}>
-        <GrCaretNext className="text-5xl  text-deep-orange-700 hover:text-4xl transition-all duration-500 hover:text-opacity-50 "/>
+        <span className="text-xl font-bold text-white  ">
+          {" "}
+          Exercise {currentPage} of {totalPages}{" "}
+        </span>
+        <button
+          className="w-10"
+          onClick={goToNextPage}
+          disabled={currentPage === totalPages}
+        >
+          <GrCaretNext className="text-5xl  text-deep-orange-700 hover:text-4xl transition-all duration-500 hover:text-opacity-50 " />
         </button>
       </div>
-      {exercisesToDisplay.map((exercise) => (
-        <Exercise key={exercise.id} data={exercise} />
+      {exercisesToDisplay?.map((exercise) => (
+        <Exercise key={exercise.id} exercise={exercise} />
       ))}
-      
+      <p className="text-white text-center text-5xl">THIS IS A TEST</p>
     </section>
   );
 }
