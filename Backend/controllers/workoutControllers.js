5dccd1ef383fdedcca9de33e4c3978f4fdf37c8b
Maziar Pedrami <awildroot@gmail.com>
2024-01-05 16:45:19 +0100

    getWorkout controller and route added

--- Backend/controllers/workoutControllers.js
+++ Backend/controllers/workoutControllers.js
@@ -1,3 +1,4 @@
+const asyncHandler = require("express-async-handler");
 const Workout = require("../models/workout");
 const User = require("../models/user");
 const Exercise = require("../models/exercise");
@@ -5,7 +6,7 @@ const Exercise = require("../models/exercise");
 const createWorkout = async (req, res, next) => {
   try {
     const {
-      body: { name, description, exercises,level, creator, thumbnail },
+      body: { name, description, exercises, level, creator, thumbnail },
     } = req;
     const newWorkout = await Workout.create({
       name,
@@ -21,15 +22,29 @@ const createWorkout = async (req, res, next) => {
   }
 };
 
+const getWorkout = asyncHandler(async (req, res) => {
+  const {
+    params: { id },
+  } = req;
+  const workout = await Workout.findById(id).populate("creator").populate("exercises");
+  if (!workout) {
+    res.status(404);
+    throw new Error("The Workout You Are Looking For Does Not Exist");
+  }
+  res
+    .status(200)
+    .json({ message: "Workout is in the database", data: workout });
+});
+
 const getWorkouts = async (req, res, next) => {
   try {
     const workouts = await Workout.find()
       .populate("creator")
       .populate("exercises");
-    res.json({workouts});
+    res.json({ workouts });
   } catch (error) {
     next(error);
   }
 };
 
-module.exports = { createWorkout, getWorkouts };
+module.exports = { createWorkout, getWorkouts, getWorkout };
--- Backend/routes/workoutRoutes.js
+++ Backend/routes/workoutRoutes.js
@@ -3,8 +3,10 @@ const router = express.Router();
 const {
   getWorkouts,
   createWorkout,
+  getWorkout,
 } = require("../controllers/workoutControllers");
 
 router.route("/").get(getWorkouts).post(createWorkout);
+router.route("/:id").get(getWorkout);
 
 module.exports = router;
