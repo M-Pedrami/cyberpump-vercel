4d79cb956b5882f9651e13d8f403d22ba1e4cfa7
Maziar Pedrami <awildroot@gmail.com>
2024-01-16 20:18:35 +0100

    conditional rendering of Navbar

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -2,10 +2,12 @@ import LOGO from "../../assets/Brand.png";
 import { Link } from "react-router-dom";
 import ProfileMenu from "../ProfileMenu";
 import { useUser } from "../../utils/UserContext";
+
 export default function Navbar() {
-  const {activeUser} =  useUser();
+  const { activeUser } = useUser();
+  const isAdmin = activeUser && activeUser.role === "admin";
+
   return (
-    
     <nav className="relative container mx-auto p-6">
       <div className="flex items-center justify-between">
         {/* LOGO */}
@@ -16,18 +18,40 @@ export default function Navbar() {
         </div>
         {/* MENU ITEMS */}
         <div className="hidden md:flex space-x-6">
-          <Link to="/workout" className= "font-bold text-orange-600 hover:text-orange-400">WORKOUTS</Link>
-          <Link to="/directory" className= "font-bold text-orange-600 hover:text-orange-400">DIRECTORY</Link>
-          <Link to="/login" className= "font-bold text-orange-600 hover:text-orange-400">ABOUT US</Link>
-          <Link to={activeUser ? "/" : "/login"} className= "font-bold text-orange-600 hover:text-orange-400">{activeUser ? "EXERCISES" : "LOG IN"}</Link>
-          <Link to="/signup" className= "font-bold text-orange-600 hover:text-orange-400">{activeUser ? "TOOLS" : "SIGN UP"}</Link>
-          <Link to="/addexercise" className= "font-bold text-orange-600 hover:text-orange-400">ADD EXERCISE</Link>
-          <Link to="/addworkout" className= "font-bold text-orange-600 hover:text-orange-400">ADD WORKOUT</Link>
-          <Link to="/dashboard/users" className= "font-bold text-orange-600 hover:text-orange-400">Dashboard</Link>
-          
+          <Link to="/workout" className="font-bold text-orange-600 hover:text-orange-400">
+            WORKOUTS
+          </Link>
+          <Link to="/directory" className="font-bold text-orange-600 hover:text-orange-400">
+            DIRECTORY
+          </Link>
+          <Link to="/login" className="font-bold text-orange-600 hover:text-orange-400">
+            ABOUT US
+          </Link>
+          <Link to={activeUser ? "/" : "/login"} className="font-bold text-orange-600 hover:text-orange-400">
+            {!activeUser &&  "LOG IN"}
+          </Link>
+          <Link to="/signup" className="font-bold text-orange-600 hover:text-orange-400">
+            {!activeUser && "SIGN UP"}
+          </Link>
+          {isAdmin && (
+            <>
+              <Link to="/addexercise" className="font-bold text-orange-600 hover:text-orange-400">
+                ADD EXERCISE
+              </Link>
+              <Link to="/addworkout" className="font-bold text-orange-600 hover:text-orange-400">
+                ADD WORKOUT
+              </Link>
+              <Link
+                to={isAdmin ? "/dashboard/users" : activeUser && activeUser.role === "user" ? "/profile" : null}
+                className="font-bold text-orange-600 hover:text-orange-400"
+              >
+                {isAdmin ? "DASHBOARD" : "MY PROFILE"}
+              </Link>
+            </>
+          )}
         </div>
         {/* Profile Menu */}
-        <ProfileMenu/>
+        <ProfileMenu />
       </div>
     </nav>
   );
