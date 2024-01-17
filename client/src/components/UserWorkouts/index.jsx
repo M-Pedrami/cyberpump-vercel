b2fb93aaae9f1dc9ced4f82279f9fdf509db228c
Maziar Pedrami <awildroot@gmail.com>
2024-01-17 12:19:39 +0100

    last commit

--- client/src/components/UserWorkouts/index.jsx
+++ client/src/components/UserWorkouts/index.jsx
@@ -10,8 +10,15 @@ export default function UserWorkoutGrid() {
     .catch(err=>(console.log("USEEFFECT WorkoutGrid Catch", err)))
   }, [])
   return (
+    
     <section className="p-6 grid grid-cols-1 lg:grid-cols-4 md:grid-cols-2 gap-4">
-   {workoutData && workoutData.map(workout=><WorkoutCard key={workout._id} data={workout}/>)}
-    </section>
+    {workoutData.length > 0 ? (
+      workoutData.map(workout => (
+        <WorkoutCard key={workout._id} data={workout} />
+      ))
+    ) : (
+      <p className="text-4xl text-white m-auto w-96">Your Requested Workouts Will Be Displayed Here </p>
+    )}
+  </section>
   )
 }
